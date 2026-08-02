2fbf2aa7721f676bb2aba81015a1fbeac843fd87
Raja Mohamed Nawfal <rajamohamednawfal@gmail.com>
2026-08-03 02:17:47 +0530

    updates

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -3,8 +3,19 @@ import { Module } from "@nestjs/common";
 import { OrdersController } from "./orders.controller";
 import { OrdersService } from "./orders.service";
 
+import { DocumentsModule } from "../documents/documents.module";
+
 @Module({
-  controllers: [OrdersController],
-  providers: [OrdersService],
+  imports: [
+    DocumentsModule,
+  ],
+
+  controllers: [
+    OrdersController,
+  ],
+
+  providers: [
+    OrdersService,
+  ],
 })
 export class OrdersModule {}
