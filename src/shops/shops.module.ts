fc03b71e0bca840a6855e9eaf157e59b10c49fef
Raja Mohamed Nawfal <rajamohamednawfal@gmail.com>
2026-08-03 11:49:35 +0530

    updates

--- src/shops/shops.module.ts
+++ src/shops/shops.module.ts
@@ -3,8 +3,19 @@ import { Module } from "@nestjs/common";
 import { ShopsController } from "./shops.controller";
 import { ShopsService } from "./shops.service";
 
+import { DocumentsModule } from "../documents/documents.module";
+
 @Module({
-  controllers: [ShopsController],
-  providers: [ShopsService],
+  imports: [
+    DocumentsModule,
+  ],
+
+  controllers: [
+    ShopsController,
+  ],
+
+  providers: [
+    ShopsService,
+  ],
 })
 export class ShopsModule {}
